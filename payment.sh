6274e604636f603cb3c1559bc71f6af5a4b25bf9
Jansi Gorle <jansigorle@Jansis-MacBook-Air.local>
2026-07-02 15:07:51 +0530

    payment-shell

--- payment.sh
+++ payment.sh
@@ -1,11 +1,11 @@
-#! bin/bash
+#! /bin/bash
 
 folder=/var/log/payment-logs
-mkdir -p $folder
-filename=$(echo $0|cut -d "." -f 1)
+mkdir -p "$folder"
+filename=$(basename "$0" .sh)
 logfile=$folder/$filename.log
-touch $logfile
-$Script_dir=$PWD
+touch "$logfile"
+script_dir=$(cd "$(dirname "$0")" && pwd)
 
 user=$(id -u)
 if [ $user -ne 0 ]; then
@@ -24,41 +24,41 @@ validate(){
 
 
 
-dnf install python3 gcc python3-devel -y &>> "$logfile"
+dnf install python3 gcc python3-devel -y >> "$logfile" 2>&1
 validate $? "installing python"
 
-id roboshop &>> "$logfile"
+id roboshop >> "$logfile" 2>&1
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /bin/bash roboshop
+    useradd --system --home /app --shell /bin/bash roboshop >> "$logfile" 2>&1
     validate $? "system user created"
 else
     validate 0 "system user already exists ...."    
 fi
 
 
-mkdir -p /app &>> "$logfile"
+mkdir -p /app >> "$logfile" 2>&1
 validate $? "move to /app directory"
 
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>> "$logfile"
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip >> "$logfile" 2>&1
 validate $? "downloading application artifact"
 
-cd /app &>> "$logfile"
+cd /app >> "$logfile" 2>&1
 validate $? "change to /app directory"
 
-unzip -o /tmp/payment.zip &>> "$logfile"
-validate $? "delete old fils in /app and unzip application artifact"
+unzip -o /tmp/payment.zip >> "$logfile" 2>&1
+validate $? "delete old files in /app and unzip application artifact"
 
-pip3 install -r requirements.txt &>> "$logfile"
+pip3 install -r requirements.txt >> "$logfile" 2>&1
+validate $? "installing python dependencies"
 
+cp "$script_dir/payment.service" /etc/systemd/system/payment.service >> "$logfile" 2>&1
+validate $? "copying payment service"
 
-cp $Script_dir/payment.service /etc/systemd/system/payment.service &>> "$logfile"
-validate $? "coping payment service"
-
-systemctl daemon-reload &>> "$logfile"
+systemctl daemon-reload >> "$logfile" 2>&1
 validate $? "reload service configurations"
 
-systemctl enable payment &>> "$logfile"
+systemctl enable payment >> "$logfile" 2>&1
 validate $? "enable payment service"
 
-systemctl start payment &>> "$logfile"
+systemctl start payment >> "$logfile" 2>&1
 validate $? "start payment service"
